6fd10deab96be7f89a4ed8b95cb903af9f97db34
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-05-15 22:23:28 -0300

    término

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,17 +1,36 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import TarefasVue from "@/view/Tarefas.vue";
 import Projetos from '@/view/Projetos.vue'
+import Formulario from '../view/Projetos/Formulario.vue'
+import Lista from '../view/Projetos/Lista.vue'
 const rotas: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'Tarefas',
         component: TarefasVue
     },
-    {
-        path: '/projetos',
-        name: 'Projetos',
-        component: Projetos
-    }
+   {
+    path: '/projetos',
+    component: Projetos,
+    children: [
+        {
+            path: '',
+            name: 'Projetos',
+            component: Lista
+        },
+        {
+            path: 'novo',
+            name: 'Novo Projeto',
+            component: Formulario
+        },
+        {
+            path: ':id',
+            name: 'Editar Projeto',
+            component: Formulario,
+            props: true
+        }
+    ]
+   }
 ]
 const roteador = createRouter({
     history: createWebHashHistory(),
